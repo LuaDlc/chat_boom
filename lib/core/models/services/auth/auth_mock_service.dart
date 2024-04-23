2266dfc700ecfe8efbdbf361f52602648014f6b3
Luana Dias <luana.dias@colab.re>
2024-04-23 14:24:05 -0300

    fix: adicionando usuario default e imagem

--- lib/core/models/services/auth/auth_mock_service.dart
+++ lib/core/models/services/auth/auth_mock_service.dart
@@ -1,30 +1,70 @@
+import 'dart:async';
 import 'dart:io';
+import 'dart:math';
 
 import 'package:chat/core/models/chat_user.dart';
 import 'package:chat/core/models/services/auth/auth_service.dart';
 
 class AuthMockService implements AuthService {
+  static final _defaultUser = ChatUser(
+    id: '1',
+    name: 'Teste',
+    email: 'teste@teste.com',
+    imageUrl: 'assets/images/avatar.png',
+  );
+  static Map<String, ChatUser> _users = {
+    _defaultUser.email: _defaultUser,
+  };
+  static ChatUser? _currentUser;
+  static MultiStreamController<ChatUser?>? _controller;
+  static final _userStream = Stream<ChatUser?>.multi((controller) {
+    _controller = controller;
+    _updateUser(_defaultUser);
+  });
   @override
   ChatUser? get currentUser {
-    return null;
+    return _currentUser;
   }
 
   @override
-  Stream<ChatUser?>
-      get userChanges {} //sempre que mudar o estado do usuario lanca um novo dado no estado do usuario
+  Stream<ChatUser?> get userChanges {
+    return _userStream;
+  } //sempre que mudar o estado do usuario lanca um novo dado no estado do usuario
 
   @override
   Future<void> signup(
     String name,
     String password,
     String email,
-    File image,
-  ) async {}
+    File? image,
+  ) async {
+    //crio um novo usuario
+    final newUser = ChatUser(
+        id: Random().nextDouble().toString(),
+        name: name,
+        email: email,
+        imageUrl: image?.path ?? 'assets/images/avatar.png');
+    //adiciona mais um usuario no map
+    _users.putIfAbsent(email, () => newUser);
+    //e agr pode fazer o update e o login chamando o update user passando o newUser
+    _updateUser(newUser);
+  }
+
   @override
   Future<void> login(
     String email,
     String password,
-  ) async {}
+  ) async {
+    _updateUser(_users[email]);
+  }
+
   @override
-  Future<void> logout() async {}
+  Future<void> logout() async {
+    _updateUser(null);
+  }
+
+  static void _updateUser(ChatUser? user) {
+    _currentUser = user;
+    _controller?.add(_currentUser);
+  }
 }
