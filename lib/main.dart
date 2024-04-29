d8b8a4cb192a9d0ecfba44a37b45c5f082ee7d1a
Luana Dias <luana.dias@colab.re>
2024-04-29 20:04:49 -0300

    fix:adicionando models notification e provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:chat/core/models/services/notification/push_notification_service.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'pages/auth_or_app_page.dart';
 
@@ -11,13 +13,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => PushNotificationService(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const AuthOrAppPage(),
+        debugShowCheckedModeBanner: false,
       ),
-      home: const AuthOrAppPage(),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
